3383faaa10db9af3da0d5479dba9df2b24dfc480
admin <admin@YD02-23>
2020-07-21 15:25:32 +0900

    D

--- sosoorder/src/main/java/com/soso/app/order/service/OrderService.java
+++ sosoorder/src/main/java/com/soso/app/order/service/OrderService.java
@@ -3,6 +3,8 @@ package com.soso.app.order.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.soso.app.menu.service.MenuSearchVO;
 
 public interface OrderService {
