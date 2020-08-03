2128d68fd35c14ec40a4f7b13a89e2252eee6599
eo339912 <eo339912@gmail.com>
2020-08-03 11:27:29 +0900

    0803_JW

---
+++ sosoorder/src/main/java/com/soso/app/order/call/HandshakeInterceptor.java
@@ -0,0 +1,36 @@
+package com.soso.app.order.call;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		// 위의 파라미터 중, attributes 에 값을 저장하면 웹소켓 핸들러 클래스의 WebSocketSession에 전달된다
+		ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
+		HttpServletRequest req = ssreq.getServletRequest();
+		String loginvo = (String) req.getSession().getAttribute("loginInfo");
+		if(  loginvo  != null )
+			attributes.put("loginvo", loginvo);
+		else {
+			attributes.put("loginvo", "kym"); //default 초기값
+		}
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception ex) {
+		System.out.println("After Handshake");
+		super.afterHandshake(request, response, wsHandler, ex);
+	}
+
+}
