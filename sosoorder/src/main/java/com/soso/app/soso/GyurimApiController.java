478e44bd00d1a88dc70c3717d9d983dc60ef15c7
mbp <mbp@192.168.1.81>
2020-08-05 12:28:19 +0900

    ddd

--- sosoorder/src/main/java/com/soso/app/soso/GyurimApiController.java
+++ sosoorder/src/main/java/com/soso/app/soso/GyurimApiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
-public class SosoApiController2 {
+public class GyurimApiController {
 	
 	@ResponseBody
 	@RequestMapping("getApi8")
